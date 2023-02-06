069dee59cf495264222f6c0d378050d6fd028476
Anthony Mugendi <ngurumugz@gmail.com>
2023-02-06 15:34:04 +0300

    debugged axios errors

--- index.js
+++ index.js
@@ -8,13 +8,12 @@
 const axios = require('axios'),
 	isAbsoluteUrl = require('is-absolute-url'),
 	isValidPath = require('is-valid-path'),
-    path = require('path'),
-    fs = require('fs');
+	path = require('path'),
+	fs = require('fs');
 
 async function to_buffer(dataOrPath) {
 	try {
-
-        // if is already a buffer
+		// if is already a buffer
 		if (Buffer.isBuffer(dataOrPath)) {
 			return dataOrPath;
 		}
@@ -26,7 +25,17 @@ async function to_buffer(dataOrPath) {
 					responseType: 'arraybuffer',
 				})
 				.catch((error) => {
-					throw error.response.statusText;
+					// throw error.response.statusText;
+					if (error.response) {
+						// Request made and server responded
+						throw error.response.data;
+					} else if (error.request) {
+						// The request was made but no response was received
+						throw error.request;
+					} else {
+						// Something happened in setting up the request that triggered an Error
+						throw error.message;
+					}
 				});
 
 			return data;
@@ -36,12 +45,15 @@ async function to_buffer(dataOrPath) {
 		if (isValidPath(dataOrPath)) {
 			let resolvedPath = path.resolve(dataOrPath);
 
-			if (fs.existsSync(resolvedPath) && fs.statSync(resolvedPath).isFile()) {
+			if (
+				fs.existsSync(resolvedPath) &&
+				fs.statSync(resolvedPath).isFile()
+			) {
 				return fs.readFileSync(resolvedPath);
 			}
 		}
 
-        // if is a string
+		// if is a string
 		if (typeof dataOrPath == 'string') {
 			return Buffer.from(dataOrPath);
 		}
